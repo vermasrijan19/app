e4dfb916dc20c0756cbf9ceca8c66c6a0a96239a
Srijan <verma.srijan19@gmail.com>
2023-02-05 19:21:22 +0530

    dasd

--- app.py
+++ app.py
@@ -85,9 +85,7 @@ def chk_accuracy():
     return {"user": user, "score": score}
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():  # put application's code here
-    return 'Hello World!'
+
 
 if __name__ == '__main__':
     app.run(debug=False, host='0.0.0.0',port=8080)
